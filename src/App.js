93c10193933aaea8811090fcdfc4b0a4ee508d4b
mbalandari <m.balandari.toroghi@gmail.com>
2022-02-15 16:10:40 +0300

    add router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Category from './pages/Category';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/category" element={<Category />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
